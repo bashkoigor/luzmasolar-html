3f7ecf9a263572a46789664ede1bab3d3568dbd7
admin <admin@mail.loc>
2022-11-01 17:43:47 +0200

    init page nuestro-equipo

---
+++ script.js
@@ -0,0 +1,9 @@
+// Recommended product carousel
+const team = document.querySelector("#team");
+if (team) {
+    const recommended1 = new Carousel(team, {
+        slidesPerPage: 1,
+        center: false,
+        Dots: false,
+    });
+}
